01929c5d3a8e7b94bda7390f7838ea2649677539
Ankit Tripathi <ankittripathi2k24@gmail.com>
2025-11-23 22:18:50 +0530

    initial commit

---
+++ 01_nextjs/app/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-3xl text-center'>
+        Let's Start
+      </h1>
+    </div>
+  )
+}
+
+export default page
